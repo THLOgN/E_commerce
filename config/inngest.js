5ce4124d984511624e9eb5b508472a668ec969fc
THLOgN <102423409+THLOgN@users.noreply.github.com>
2025-09-15 18:07:48 +0700

    first commit

---
+++ config/inngest.js
@@ -0,0 +1,28 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "quickcart_next" });
+
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync/user/creation",
+  },
+  {
+    event: "Clerk/user.created",
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: `${first_name} ${last_name}`,
+      imageUrl: image_url,
+    };
+    await connectDB();
+    await User.create(userData);
+    console.log("User created:", userData);
+  }
+);
